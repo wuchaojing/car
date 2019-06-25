7cf1bff6f10924748c1973a0793b9239a482dde6
xingyi <1160330282@qq.com>
2019-06-25 17:17:00 +0800

    临时push：完善 review_state<>'已注销'

--- src/main/java/com/car/demo/util/ConstantUtil.java
+++ src/main/java/com/car/demo/util/ConstantUtil.java
@@ -6,8 +6,8 @@ public class ConstantUtil {
 
 //    public final static String UPLOAD_PATH = "D:/photo_xingyi_excel2/data";
 
-    public final static String UPLOAD_PATH = "/data";
-//    public final static String UPLOAD_PATH = "D:/photo_xingyi_excel2/data";
+    //    public final static String UPLOAD_PATH = "/data";
+    public final static String UPLOAD_PATH = "D:/photo_xingyi_excel2/data";
 
 
 
